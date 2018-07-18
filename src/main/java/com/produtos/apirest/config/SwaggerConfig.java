1c9820ba414ca6e479407bced1c381e52a7aa580
michellibrito <michellidibrito@gmail.com>
2018-07-18 13:28:40 -0300

    inserindo Swagger-ui

---
+++ src/main/java/com/produtos/apirest/config/SwaggerConfig.java
@@ -0,0 +1,49 @@
+package com.produtos.apirest.config;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.produtos.apirest"))
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "Produtos API REST",
+                "API REST de cadastro de produtos.",
+                "1.0",
+                "Terms of Service",
+                new Contact("Michelli Brito", "https://www.youtube.com/michellibrito",
+                        "michellidibrito@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
+        );
+
+        return apiInfo;
+    }
+
+}
--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -18,33 +18,42 @@ import org.springframework.web.bind.annotation.RestController;
 import com.produtos.apirest.models.Produto;
 import com.produtos.apirest.repository.ProdutoRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value="/api")
+@Api(value="API REST Produtos")
 public class ProdutoResource {
 	
 	@Autowired
 	ProdutoRepository produtoRepository;
 	
+	@ApiOperation(value="Retorna uma lista de Produtos")
 	@GetMapping("/produtos")
 	public List<Produto> listaProdutos(){
 		return produtoRepository.findAll();
 	}
 	
+	@ApiOperation(value="Retorna um produto unico")
 	@GetMapping("/produto/{id}")
 	public Produto listaProdutoUnico(@PathVariable(value="id") long id){
 		return produtoRepository.findById(id);
 	}
 	
+	@ApiOperation(value="Salva um produto")
 	@PostMapping("/produto")
 	public Produto salvaProduto(@RequestBody @Valid Produto produto) {
 		return produtoRepository.save(produto);
 	}
 	
+	@ApiOperation(value="Deleta um produto")
 	@DeleteMapping("/produto")
 	public void deletaProduto(@RequestBody @Valid Produto produto) {
 		produtoRepository.delete(produto);
 	}
 	
+	@ApiOperation(value="Atualiza um produto")
 	@PutMapping("/produto")
 	public Produto atualizaProduto(@RequestBody @Valid Produto produto) {
 		return produtoRepository.save(produto);
